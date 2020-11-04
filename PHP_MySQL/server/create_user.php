aa0f07de1e31a9fb49cf9642ede7f047855678e9
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-11-04 16:40:06 -0600

    Module 7 - Last Project - PHP-MySQL - points 1,2

--- PHP_MySQL/server/create_user.php
+++ PHP_MySQL/server/create_user.php
@@ -1,6 +1,82 @@
 <?php
 
+  require('lib.php');
 
+  # Creating DB Object
+  $con = new ConectorBD('localhost','nextu','12345');
+
+  # Getting connection into agenda_db database
+  $message = $con->initConexion('agenda_db');
+
+  echo "<h3>Status conexión a agenda_db:</h3><b>".$message."</b><br><br>";
+
+	# To use encrypt passwords functions
+  #password_hash(, PASSWORD_DEFAULT);
+  #password_verify($_POST['passw'], $fila['psw']);
+
+  #################### Creating User 1
+
+  $email = "'jps@mail.com'";
+  $nombre = "'Juan Perez S'";
+  $passwd = "'".password_hash('12345678', PASSWORD_DEFAULT)."'";
+  $birth_date = "'1989-09-11'";
+
+  $user =['email' => $email,
+          'nombre_completo' => $nombre,
+          'password' => $passwd ,
+          'fecha_nacimiento' => $birth_date];
+
+  $query = $con->insertUsers($user);
+
+  echo "Creando usuario: ".$email."<br>";
+  echo $query."<br><br>";
+
+  if(!$con->ejecutarQuery($query)){
+	  echo "Error en creación de usuario <b>".$email."</b>!<br><br>";
+  };
+
+  #################### Creating User 2
+
+  $email = "'klc@mail.com'";
+  $nombre = "'Karla Luisa C'";
+  $passwd = "'".password_hash('12345', PASSWORD_DEFAULT)."'";
+  $birth_date = "'1999-06-23'";
+
+  $user =['email' => $email,
+          'nombre_completo' => $nombre,
+          'password' => $passwd ,
+          'fecha_nacimiento' => $birth_date];
+
+  $query = $con->insertUsers($user);
+
+  echo "Creando usuario: ".$email."<br>";
+  echo $query."<br><br>";
+#  $con->ejecutarQuery($query);
+
+  if(!$con->ejecutarQuery($query)){
+	  echo "Error en creación de usuario <b>".$email."</b>!<br><br>";
+  };
+
+  #################### Creating User 3
+
+  $email = "'mmr@mail.com'";
+  $nombre = "'Manuel Martin R'";
+  $passwd = "'".password_hash('123456789', PASSWORD_DEFAULT)."'";
+  $birth_date = "'1985-01-15'";
+
+  $user =['email' => $email,
+          'nombre_completo' => $nombre,
+          'password' => $passwd ,
+          'fecha_nacimiento' => $birth_date];
+
+  $query = $con->insertUsers($user);
+
+  echo "Creando usuario: ".$email."<br>";
+  echo $query."<br><br>";
+
+  if(!$con->ejecutarQuery($query)){
+	  echo "Error en creación de usuario <b>".$email."</b>!<br><br>";
+  };
 
 
 
---
+++ PHP_MySQL/server/lib.php
@@ -0,0 +1,61 @@
+<?php
+
+
+class ConectorBD
+{
+  private $host;
+  private $user;
+  private $password;
+  private $conexion;
+
+  function __construct($host, $user, $password){
+    $this->host = $host;
+    $this->user = $user;
+    $this->password = $password;
+  }
+
+  # Function to Connect to DB response with Error or OK messages
+  function initConexion($nombre_db){
+    $this->conexion = new mysqli($this->host, $this->user, $this->password, $nombre_db);
+    if ($this->conexion->connect_error) {
+      return "Error:" . $this->conexion->connect_error;
+    }else {
+      return "OK";
+    }
+  }
+
+  # Function to execute a query
+  function ejecutarQuery($query){
+    return $this->conexion->query($query);
+  }
+
+  # Function to create an user in users table in agenda_db
+  function insertUsers($columns){
+    $sql = 'INSERT INTO users'.'(';
+    $i = 1;
+    foreach ($columns as $key => $value) {
+      $sql .= $key;
+      if ($i<count($columns)) {
+        $sql .= ', ';
+      }else $sql .= ')';
+      $i++;
+    }
+    $sql .= ' VALUES (';
+    $i = 1;
+    foreach ($columns as $key => $value) {
+      $sql .= $value;
+      if ($i<count($columns)) {
+        $sql .= ', ';
+      }else $sql .= ');';
+      $i++;
+    }
+
+    return $sql;
+    #return $this->ejecutarQuery($sql);
+
+  }
+
+}
+
+
+?>
