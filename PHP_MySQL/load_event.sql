53f7da074012796c9f19b78ecdcef8cb9719871d
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2020-11-10 10:08:26 -0600

    Module 7 - Last Project - NodeJs-MongoDB - starting

---
+++ PHP_MySQL/load_event.sql
@@ -0,0 +1,16 @@
+INSERT INTO agenda_db.evento 
+(titulo,
+fecha_inicio,
+hora_inicio,
+fecha_fin,
+hora_fin,
+dia_completo,
+user_id) 
+VALUES 
+('Primer tarea',
+'2020-11-15 00:00:00',
+'12:00:00',
+'2020-11-17 00:00:00',
+'14:00:00',
+0,
+9);
